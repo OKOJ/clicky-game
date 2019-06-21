ff03505484fd83407da105ebfd2d4f880ba47404
OKOJ <aajmd@hotmail.com>
2019-06-20 20:56:42 -0600

    components

---
+++ src/components/Game/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import Navbar from "../Navbar";
+import Container from "../Container";
+import Footer from "../Footer";
+import data from "../../data.json";
+
+class Game extends Component{
+    state = {
+       // data,
+        score:0,
+        topScore:0
+    }
+}
+
+
+export default Game;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar(props) {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li className="brand"> 
+                    <a href="/">Clicky Game</a>
+                </li>
+
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
